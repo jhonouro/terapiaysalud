39331d9b679ee728fc35e75b5003b10f7acf6f6e
jhonouro <jhonouro@gmail.com>
2020-06-07 13:46:38 -0500

    Actividad 7

---
+++ js/materialize.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems);
+});
